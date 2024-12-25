aa01975802cd35a83ac87c0670cda9607377ac17
DiegoMillanOrtega <diegomillan1005@gmail.com>
2024-12-25 09:27:20 -0500

    Manejo de Errores >> Se implementó en el interceptor el manejo de errores Http >> En el componente agregar-tercero se implemento el manejo de error para cuando se intenta guardar un tercero ya existente con mismo Tipo Documento y Nit.

--- src/app/core/interceptor/http.interceptor.ts
+++ src/app/core/interceptor/http.interceptor.ts
@@ -1,15 +1,105 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpHeaders, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../service/auth.service';
+import { catchError, throwError } from 'rxjs';
+import { Toast } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 export const httpInterceptor: HttpInterceptorFn = (req, next) => {
 
+  
   const authService = inject(AuthService);
+  const messageService = inject(MessageService);
 
+  if (!authService) {
+    console.warn('AuthService not initialized yet.');
+    return next(req);
+  }
+
+  console.log(authService.isAuthenticated());
   if (authService.isAuthenticated()) {
+    console.log(localStorage.getItem('authToken'));
+    
     req = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${localStorage.getItem('authToken')}`)
+      headers: req.headers.set('Authorization', `Bearer ${localStorage.getItem('authToken')}`) || new HttpHeaders()
     });
   }
-  return next(req);
+  return next(req)
+  .pipe(
+    catchError((error: HttpErrorResponse) => {
+      console.log('HTTP Error Response', error);
+
+      switch (error.status) {
+        case 401:
+          console.log('HTTP Error Response 401', error);
+
+          messageService.add({
+            severity: 'error',
+            summary: 'Unauthorized',
+            detail: 'No tienes permiso para acceder a este recurso.',
+          });
+
+          break;
+
+        case 403:
+          console.log('HTTP Error Response 403', error);
+
+          messageService.add({
+            severity: 'error',
+            summary: 'Forbidden',
+            detail: 'Acceso denegado.',
+          });
+
+          break;
+
+          case 404:
+            console.log('HTTP Error Response 404', error);
+  
+            messageService.add({
+              severity: 'warn',
+              summary: 'Not Found',
+              detail: 'El recurso solicitado no existe.',
+            });
+  
+            break;
+
+          case 409:
+            console.log('HTTP Error Response 409', error);
+  
+            messageService.add({
+              severity: 'warn',
+              summary: 'Conflicto',
+              detail: 'El recurso solicitado ya existe.',
+            });
+  
+            break;
+          
+        case 500:
+          console.log('HTTP Error Response 500', error);
+
+          messageService.add({
+            severity: 'error',
+            summary: 'Server Error',
+            detail: 'Ocurrió un error en el servidor. Por favor, intenta nuevamente más tarde.',
+          });
+
+          break;
+
+        default:
+          console.log('HTTP Error Response default', error);
+
+          messageService.add({
+            severity: 'info',
+            summary: 'Error',
+            detail: error.message || 'Algo salió mal. Por favor, intenta nuevamente.',
+          });
+          break;
+      }
+
+      return throwError(() => error);
+    })
+  );
+
+
+
 };
